5722ca619144962fbf59ef621550d375cae4c68e
redwan-hajji <you@example.com>
2023-11-02 13:59:09 +0200

    first commit

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+menuIcon=document.querySelector(".menu-icon");
+menu=document.querySelector(".menu");
+
+menuIcon.addEventListener("click",function(){
+ menu.classList.toggle("hidden"); 
+  menu.classList.toggle("block");
+})
+
